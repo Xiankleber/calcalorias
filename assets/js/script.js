0ee0c3665cf4646c8e8bd2adba257c5493b46656
Xiankleber <Xiankleber>
2021-09-28 17:27:54 -0300

    Kcal

---
+++ assets/js/script.js
@@ -0,0 +1,66 @@
+// // Ouvindo o evento atarvés do addEventListener
+// // A função vai ser executada  toda vez que utilizarmos o submit
+// // event previne toda vez de reccaregar a página
+
+const form = document.getElementById("form");
+form.addEventListener("submit", handleSubmit);
+
+//função  enviar
+// Passnado a função
+function handleSubmit(event) {
+    event.preventDefault();
+
+    //  Pegando os valores do input getinput
+    //Pegando os valores com numeros getinputnumber
+
+    const gender = getSelectedValue("gender");
+    const age = getInputNumberValue("age");
+    const weight = getInputNumberValue("weight");
+    const height = getInputNumberValue("height");
+    const activityLevel = getSelectedValue("activity_level");
+    // Operçãopara calcular o valor das calorias
+    const tmb = Math.round(
+        gender === "female" ?
+        655 + 9.6 * weight + 1.8 * height - 4.7 * age :
+        66 + 13.7 * weight + 5 * height - 6.8 * age
+    );
+    //
+    const maintenance = Math.round(tmb * Number(activityLevel));
+    const loseWeight = maintenance - 450;
+    const gainWeight = maintenance + 450;
+
+    const layout = `
+    <h2>Resultado:</h2>
+
+    <div class="result-content">
+      <ul>
+        <li>
+          Seu metabolismo basal é :${tmb} calorias</strong>.
+        </li>
+        <li>
+          Para manter o seu peso: <strong>${maintenance} calorias</strong>.
+        </li>
+        <li>
+          Para perder peso  sua média diaria é: <strong>${loseWeight} calorias</strong>.
+        </li>
+        <li>
+          Ganhando peso média diária: <strong>${gainWeight} calorias</strong>.
+        </li>
+      </ul>
+    </div>
+  `;
+
+    // Retornandoo valor o resultado
+    const result = document.getElementById("result");
+
+    result.innerHTML = layout;
+}
+
+function getSelectedValue(id) {
+    const select = document.getElementById(id);
+    return select.options[select.selectedIndex].value;
+}
+
+function getInputNumberValue(id) {
+    return Number(document.getElementById(id).value);
+}
